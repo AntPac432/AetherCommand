a7c4334359452f428d359e892d74f6238f6163e7
AntPac432 <anthonymaysonet11@gmail.com>
2026-06-14 22:31:33 -0400

    feat: standalone — src/lib/query-client.js

--- src/lib/query-client.js
+++ src/lib/query-client.js
@@ -1,4 +1,2 @@
 import { QueryClient } from '@tanstack/react-query';
-export const queryClientInstance = new QueryClient({
-  defaultOptions: { queries: { retry: 1, staleTime: 10000 } },
-});
+export const queryClientInstance = new QueryClient({ defaultOptions: { queries: { retry: 1, staleTime: 10000 } } });
